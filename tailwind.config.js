e85dd6e655406da7fb9084232a8c4a2a436d639c
McCambley <jakemccamb16@gmail.com>
2023-04-19 01:04:25 -0400

    ✨ Create mock menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      backgroundColor: {
+        'pink': '#EDADC7',
       },
+      fontFamily: {
+        'roboto': ['Roboto Slab', 'serif'],
+      }
     },
   },
   plugins: [],
